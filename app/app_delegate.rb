d869d58e8efa5b14a4044ba6c49868117a2fc74c
jeremiahlukus <jeremiahlukus1@gmail.com>
2018-02-04 14:37:43 -0500

    First

---
+++ app/app_delegate.rb
@@ -0,0 +1,51 @@
+class AppDelegate
+  def application(application, didFinishLaunchingWithOptions:launchOptions)
+    rootViewController = UIViewController.alloc.init
+    rootViewController.title = 'HelloWorld'
+    rootViewController.view.backgroundColor = UIColor.whiteColor
+
+    navigationController = UINavigationController.alloc.initWithRootViewController(rootViewController)
+
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window.rootViewController = navigationController
+    @window.makeKeyAndVisible
+
+    # alert = UIAlertView.new
+    # alert.message = "Hello Motion!"
+    # alert.show
+    # puts "Hello again!"
+
+    #(x, y , w, h) 
+    # @blue_view = UIView.alloc.initWithFrame(CGRectMake(10, 10, 100, 100))
+    # @blue_view.backgroundColor = UIColor.blueColor
+    # @window.addSubview(@blue_view)
+
+    # @green_view = UIView.alloc.initWithFrame(CGRectMake(30, 30, 40, 40))
+    # @green_view.backgroundColor = UIColor.greenColor
+    # @window.addSubview(@green_view)
+
+    # @red_view  = UIView.alloc.initWithFrame(CGRectMake(30, 30, 40, 40))
+    # @red_view.backgroundColor = UIColor.redColor
+    # # NOTE: *not* adding to @window
+    # @blue_view.addSubview(@red_view)
+
+
+    controller = TapController.alloc.initWithNibName(nil, bundle: nil)
+    #@window.rootViewController = UINavigationController.alloc.initWithRootViewController(controller)
+    nav_controller = UINavigationController.alloc.initWithRootViewController(controller)
+
+    tab_controller = UITabBarController.alloc.initWithNibName(nil, bundle: nil)
+    tab_controller.viewControllers = [nav_controller]
+#    @window.rootViewController = tab_controller
+
+    other_controller = UIViewController.alloc.initWithNibName(nil, bundle: nil)
+    other_controller.title = "Other"
+    other_controller.view.backgroundColor = UIColor.purpleColor
+
+    tab_controller = UITabBarController.alloc.initWithNibName(nil, bundle: nil)
+    tab_controller.viewControllers = [nav_controller, other_controller]
+    @window.rootViewController = tab_controller 
+
+    true
+  end
+end
