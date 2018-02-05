20c2a748f0ed08bd3373c755c416a4233f2a8c30
jeremiahlukus <jeremiahlukus1@gmail.com>
2018-02-04 20:32:54 -0500

    Finished tables tab

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -30,21 +30,30 @@ class AppDelegate
     # @blue_view.addSubview(@red_view)
 
 
+    #controller = TapController.alloc.initWithNibName(nil, bundle: nil)
+    ##@window.rootViewController = UINavigationController.alloc.initWithRootViewController(controller)
+    #nav_controller = UINavigationController.alloc.initWithRootViewController(controller)
+
+    #tab_controller = UITabBarController.alloc.initWithNibName(nil, bundle: nil)
+    #tab_controller.viewControllers = [nav_controller]
+##    @window.rootViewController = tab_controller
+
+    #other_controller = UIViewController.alloc.initWithNibName(nil, bundle: nil)
+    #other_controller.title = "Other"
+    #other_controller.view.backgroundColor = UIColor.purpleColor
+
+    #tab_controller = UITabBarController.alloc.initWithNibName(nil, bundle: nil)
+    #tab_controller.viewControllers = [nav_controller, other_controller]
+    #@window.rootViewController = tab_controller 
+
+
     controller = TapController.alloc.initWithNibName(nil, bundle: nil)
-    #@window.rootViewController = UINavigationController.alloc.initWithRootViewController(controller)
     nav_controller = UINavigationController.alloc.initWithRootViewController(controller)
+    alphabet_controller = AlphabetController.alloc.initWithNibName(nil, bundle: nil)
 
     tab_controller = UITabBarController.alloc.initWithNibName(nil, bundle: nil)
-    tab_controller.viewControllers = [nav_controller]
-#    @window.rootViewController = tab_controller
-
-    other_controller = UIViewController.alloc.initWithNibName(nil, bundle: nil)
-    other_controller.title = "Other"
-    other_controller.view.backgroundColor = UIColor.purpleColor
-
-    tab_controller = UITabBarController.alloc.initWithNibName(nil, bundle: nil)
-    tab_controller.viewControllers = [nav_controller, other_controller]
-    @window.rootViewController = tab_controller 
+    tab_controller.viewControllers = [alphabet_controller, nav_controller]
+    @window.rootViewController = tab_controller
 
     true
   end
---
+++ app/controllers/alphabet_controller.rb
@@ -0,0 +1,43 @@
+class AlphabetController < UIViewController
+  def viewDidLoad
+    super
+
+    self.title = "Alphabet"
+
+    @table = UITableView.alloc.initWithFrame(self.view.bounds)
+    self.view.addSubview @table
+    @table.dataSource = self
+    @data = ("A".."Z").to_a
+    @table.dataSource = self
+    @table.delegate = self
+  end
+
+  def tableView(tableView, didSelectRowAtIndexPath:indexPath)
+    tableView.deselectRowAtIndexPath(indexPath, animated: true)
+
+    alert = UIAlertView.alloc.init
+    alert.message = "#{@data[indexPath.row]} tapped!"
+    alert.addButtonWithTitle "OK"
+    alert.show
+  end
+
+  def tableView(tableView, cellForRowAtIndexPath: indexPath)
+    @reuseIdentifier ||= "CELL_IDENTIFIER"
+
+    cell = tableView.dequeueReusableCellWithIdentifier(@reuseIdentifier) || begin
+    UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier:@reuseIdentifier)
+    end
+
+    cell.textLabel.text = @data[indexPath.row]
+
+    # put your data in the cell
+
+    cell
+  end
+
+  def tableView(tableView, numberOfRowsInSection: section)
+    # return the number of rows
+    @data.count 
+  end
+end
+
